55ec80e1f380dc32f93ffdd3500af65a8f43d458
DamirDenis-Tudor <denis-tudor.damir@student.tuiasi.ro>
2023-03-04 08:47:18 +0200

    The game timers has been added

---
+++ src/Timing/Timer.java
@@ -0,0 +1,58 @@
+package Timing;
+
+/**
+ *  This class provides the basic functionality of a timer.
+ *  For proper use, it should be used in a continuous loop with a specific frame rate.
+ *  The default period is 60 frames per second (fps).
+ */
+public class Timer {
+    public static final float DELTA_TIME = 0.016666668F; // default value for frame period
+    private float startTime;
+    private float endTime;
+    private float duration; // variable for saving the timer duration
+
+    /**
+     * @param duration specific to a timer
+     */
+    public Timer(float duration) {
+        this.duration = duration;
+        this.startTime = 0.0F;
+        this.endTime = 0.0F;
+    }
+
+    /**
+     * @return true/false if timer hasn't/has reached to the target
+     */
+    public boolean getTimerState() {
+        if (this.startTime >= this.endTime) {
+            return false;
+        } else {
+            this.startTime += 0.016666668F;
+            return true;
+        }
+    }
+
+    /**
+     * reset the startTime and endTime
+     * @note : if this method isn't called, the timer functionality will stop.
+     */
+    public void resetTimer() {
+        this.startTime = 0.0F;
+        this.endTime = this.duration;
+    }
+
+    /**
+     * @param newDuration the new timer target
+     */
+    public void changeDuration(float newDuration) {
+        this.duration = newDuration;
+    }
+
+    /**
+     *
+     * @return current timer situation
+     */
+    public float getRemainingTime() {
+        return this.startTime;
+    }
+}
---
+++ src/Timing/TimersHandler.java
@@ -0,0 +1,60 @@
+package Timing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class implements the Singleton design pattern and
+ * handles all the timers in the game by saving them into a HashMap.
+ */
+public class TimersHandler {
+    private Map<String, Timer> timers ; // timers HashMap
+    private static TimersHandler instance = null; // first and last instance
+
+    /**
+        This constructor initialize the HashMap.
+     */
+    private TimersHandler() {
+        timers = new HashMap<>();
+    }
+
+    /**
+        @return : The only possible instance of this class
+     */
+    public static TimersHandler getInstance() {
+        if (instance == null) {
+            instance = new TimersHandler();
+        }
+
+        return instance;
+    }
+
+    /**
+     * @param timer   timer to be added
+     * @param ID      identifier of the timer
+     * @return        void
+     * @throws        Exception
+     */
+    public void addTimer(Timer timer, String ID) throws Exception {
+        if (timer == null) {
+            throw new NullPointerException();
+        } else if (this.timers.containsKey(ID)) {
+            throw new Exception("Error : trying to add an element that have same ID with an existing one!");
+        } else {
+            this.timers.put(ID, timer);
+        }
+    }
+
+    /**
+     * @param ID timer identifier
+     * @return timer
+     * @throws Exception message
+     */
+    public Timer getTimer(String ID) throws Exception {
+        if (this.timers.containsKey(ID)) {
+            return (Timer)this.timers.get(ID);
+        } else {
+            throw new Exception("Invalid member ID in TimeHandler!");
+        }
+    }
+}
